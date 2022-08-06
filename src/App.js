459633a99d388c1151bab449d84b166f43cec48d
Aheed Shah <aheed@duck.com>
2022-08-06 14:01:07 +0100

    feat: add searching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {Component} from "react";
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,11 +13,11 @@ class App extends Component {
     componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
-            .then((users)=>this.setState(
+            .then((users) => this.setState(
                 () => {
                     return {monsters:users}
                 },
-                ()=> {
+                () => {
                     console.log(this.state)
                 }
             ))
@@ -32,17 +31,21 @@ class App extends Component {
                         return monster.name.toLocaleLowerCase().includes(event.target.value);
                     });
                     this.setState(() => {
-                        return {monsters:filteredMonsters}
+                        return { monsters:filteredMonsters }
                     })
                 }} />
                 {
                     this.state.monsters.map((monster) => {
-                        return <div key={monster.id}><h1>{monster.name}</h1></div>;
+                        return (
+                            <div key={ monster.id }>
+                                <h1> { monster.name } </h1>
+                            </div>
+                        );
                     })
                 }
             </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
